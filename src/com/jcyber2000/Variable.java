ba86f2fcfbf3e4c7c695d236739c05110b65eabd
jcovington16 <covingtonjoshua7@gmail.com>
2021-01-21 22:29:26 -0700

    Understanding that we must initialize our variables.

---
+++ src/com/jcyber2000/Variable.java
@@ -0,0 +1,10 @@
+package com.jcyber2000;
+
+public class Variable {
+    public static void main(String[] args) {
+        // We must initialize our variables
+        int myAge = 28;
+        int herAge = myAge;
+        System.out.println(myAge);
+    }
+}
